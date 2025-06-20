1e5bdd4ce0c41114b1784a489359754265199583
yunitarosadiya <yunitarosadiya05@gmail.com>
2025-06-20 19:52:15 +0700

    two commit

---
+++ app/_layout.jsx
@@ -0,0 +1,63 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState, useRef } from 'react';
+import { View, StyleSheet, Animated, Dimensions, Image} from 'react-native';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+    const colorScheme = useColorScheme();
+    const [ appReady, setAppReady ] = useState(false);
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    useEffect(() =>  {
+        Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 1500,
+            useNativeDriver: true,
+        }).start();
+
+        setTimeout(async () => {
+            await SplashScreen.hideAsync();
+            setAppReady(true);
+        }, 3000);
+    }, []);
+
+    if (!appReady) {
+        return (
+            <View style={StyleSheet.splashContainer}>
+                <Animated.Image
+                    source={require('../assets/images/logo.png')}
+                    style={[StyleSheet.logo, { opacity: fadeAnim }]}
+                    resizeMode="contain"
+                />
+            </View>
+        );
+    }
+
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Slot />
+            <StatusBar style="auto" />
+        </ThemeProvider>
+    );
+}
+
+const { width, height } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+    splashContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+    },
+
+    logo: {
+        width: width * 0.95,
+        height: height * 0.95,
+    },
+});
---
+++ app/index.jsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, ScrollView, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+
+export default function Index() {
+    const [doa, setDoa] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchDoa = async () => {
+            try {
+                const response = await fetch('https://open-api.my.id/api/doa');
+                const json = await response.json();
+                console.log('DATA APIA:', json);
+                
+                if (json && json.length > 0) {
+                    const randomIndex = Math.floor(Math.random() * json.length);
+                    setDoa(json[randomIndex]);
+                } else {
+                    console.log('Data kosong atau format tidak sesuai');
+                }
+
+                setLoading(false);
+            }   catch (error) {
+                console.error('Gagal mengambil daya doa: ', error);
+                setLoading(false);
+            }
+        };
+
+        fetchDoa();
+    }, []);
+
+    if (loading) {
+        return <ActivityIndicator size = "large" color = "#843c3c" style={{ marginTop: 50}} />;
+    }
+
+    return (
+        <ScrollView contentContainerStyle={StyleSheet.container}>
+            <Text style={StyleSheet.header}>Hafalkan Doa Ini</Text>
+
+            {doa ? (
+                <View style={StyleSheet.card}>
+                    <Text style={StyleSheet.label}>Judul : </Text>
+                    <Text style={StyleSheet.value}>{doa.judul}</Text>
+
+                    <Text style={StyleSheet.label}>Arab : </Text>
+                    <Text style={StyleSheet.value}>{doa.arab}</Text>
+
+                    <Text style={StyleSheet.label}>Latin : </Text>
+                    <Text style={StyleSheet.value}>{doa.latin}</Text>
+
+                    <Text style={StyleSheet.label}>Terjemah : </Text>
+                    <Text style={StyleSheet.value}>{doa.terjemah}</Text>
+                </View>
+            )   : (
+                <Text style={{ color: 'red', marginTop: 20 }}>Data tidak ditemukan</Text>
+            )}
+
+            <View style={StyleSheet.buttonContainer}>
+                <TouchableOpacity style={StyleSheet.buttonDoa} onPress={() => router.push('/semuadoa')}>
+                    <Text style={StyleSheet.buttonText}>SEMUA DOA</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={StyleSheet.buttonMotivasi} onPress={() => router.push('/motivasi')}>
+                    <Text style={StyleSheet.buttonText}>MOTIVASI</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        backgroundColor: '#843c3c',
+        flexGrow: 1,
+        alignItems: 'center',
+        paddingTop: 40,
+    },
+    header: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        backgroundColor: '#5e2e2e',
+        color: '#fff',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        overflow: 'hidden',
+        textAlign: 'center',
+    },
+
+    card: {
+        backgroundColor: '#a15d5d',
+        padding: 20,
+        borderRadius: 20,
+        width: '100%',
+        marginBottom: 30,
+    },
+
+    label: {
+        color: '#fff',
+        fontWeight: 'bold',
+        marginBottom: 5,
+        marginTop: 10,
+        fontSize: 16,
+    },
+
+    value: {
+        color: '#fff',
+        fontSize: 15,
+    },
+
+    buttonContainer: {
+        width: '100%',
+        gap: 15,
+        marginBotttom: 30,
+    },
+
+    buttonDoa: {
+        backgroundColor: '#5e2e2e',
+        paddingVertical: 15,
+        borderRadius: 20,
+        alignItems: 'center',
+    },
+
+    buttonMotivasi: {
+        backgroundColor: '#5e2e2e',
+        paddingVertical: 15,
+        borderRadius: 20,
+        alignItems: 'center',
+    },
+
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
